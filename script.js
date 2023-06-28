3016daf702ce07b9e536148403f64d6bacc033fd
diegoayaladuarte <138071237+diegoayaladuarte@users.noreply.github.com>
2023-06-28 17:00:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var firstname = "Diego"
+var lastname = "Ayala"
+var numberOSibling = 5;
+var haspets = true;
+
+console.log(firstname)
+console.log(lastname)
+console.log(numberOSibling)
+console.log(haspets)
+if(haspets === true){
+    console.log("SRT DEALER SHIP");
+}
+ 
+var button = document.getElementById("hi");
+button.addEventListener("click", function(){   
+console.log("click")
+})
